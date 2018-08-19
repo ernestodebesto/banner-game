d70a32fcdbb2854ab103b918c6840ea1837ad1f2
ernestodebesto <filip.sikor@gmail.comgitgit config --global user.email filip.sikor@gmail.com>
2018-08-19 19:57:50 +0200

    timer

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
-// function move() {
-//     var elem = document.getElementById("myBar");
-//     var width = 1;
-//     var id = setInterval(frame, 10);
-//     function frame() {
-//         if (width >= 100) {
-//             clearInterval(id);
-//         } else {
-//             width++;
-//             elem.style.width = width + '%';
-//         }
-//     }
-// }
+
 
 function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
@@ -23,27 +11,34 @@ function shuffle(a) {
 const PAIR_GROUPS = [
   ['football', 'basketball', 'baseball'],
   ['picasso', 'dali', 'gogh'],
-  ['chopin', 'mozart'],
-  ['africa', 'asia'],
-  ['pig', 'sheep', 'cow'] ]
+  ['pig', 'sheep', 'cow'] ];
+
+  // ['chopin', 'mozart'],
+  // ['africa', 'asia'],
 
 class Game {
   constructor(){
     this.lives = 3;
-    this.currentIndex = 1;
-    this.playTime = 20;
+    this.currentIndex = 0;
     this.selected = null;
   }
 
-  startGame() {
-
+  removeSelection(arr) {
+    for (let el of arr) {
+      el.classList.remove('image-border')
+    }
   }
 
   renderNewRound() {
+    this.handleTimer();
+    $('.counter span')[0].innerHTML = this.currentIndex + 1;
+    this.selected = null;
     //dom elements
     const imageContainers = $('.image-container');
     const wordContainers = $('.body__word-field');
-
+    //TODO to delete later when animation
+    this.removeSelection(imageContainers);
+    //
     const toDisplayGroup = PAIR_GROUPS[this.currentIndex];
 
     const shuffledWords = toDisplayGroup.slice();
@@ -56,16 +51,24 @@ class Game {
       const imageContainer = imageContainers[index];
       const wordContainer = wordContainers[index];
 
-      imageContainer.classList.add(toDisplayGroup[index]);
+      if (imageContainer.classList.length === 2) {
+        imageContainer.classList.remove(imageContainer.classList.item(1))
+        imageContainer.classList.add(toDisplayGroup[index])
+      } else {
+        imageContainer.classList.add(toDisplayGroup[index]);
+      }
       imageContainer.dataset.name = toDisplayGroup[index];
       //add to dataset
       wordContainer.lastElementChild.innerHTML = shuffledWords[index];
+
+
       imageContainer.addEventListener('click', () => {
         this.toggleBorder(imageContainer);
         this.selected = imageContainer.dataset.name;
       })
 
-      wordContainer.addEventListener('click', () => {
+      wordContainer.addEventListener('click', (e) => {
+        e.stopImmediatePropagation();
         if (this.selected) {
           this.checkPair(wordContainer)
         }
@@ -83,22 +86,40 @@ class Game {
   }
 
   checkPair(element) {
+    this.checkEndGame();
     const word = element.lastElementChild.innerHTML;
     if (this.selected === word) {
-      console.log('mamy parke');
+      if (this.currentIndex + 1 === PAIR_GROUPS.length) {
+        this.endGame()
+      } else {
+        this.currentIndex += 1;
+        this.renderNewRound();
+      }
+    } else {
+      this.looseLife();
     }
+  }
+
+  looseLife() {
+    this.lives -= 1;
+    const hearts = $('.header__heart--full');
+
+    hearts[hearts.length - 1].classList.remove('header__heart--full');
+    hearts[hearts.length - 1].classList.add('header__heart--empty');
+    this.checkEndGame();
   }
 
   toggleBorder(element) {
     if (this.selected && element.dataset.name !== this.selected) {
       const imageContainers = $('.image-container');
       for (let container of imageContainers) {
-        if (container.dataset.name === this.selected) {
-          container.style.border = 'none';
-        }
+        // if (container.dataset.name === this.selected) {
+        //   container.style.border = 'none';
+        // }
+        container.classList.remove('image-border')
       }
     }
-    element.style.border = '7px solid black'
+    element.classList.add('image-border');
   }
 
   checkMove () {
@@ -106,13 +127,41 @@ class Game {
   }
 
   checkEndGame() {
-    if (this.lives === 0 || this.playTime === 0) {
-      this.endGame()
+    if (this.lives === 0 || this.currentIndex === PAIR_GROUPS.length) {
+      this.endGame();
     }
   }
 
   endGame() {
-    console.log('game end');
+    this.currentIndex = 0;
+    this.lives = 3;
+    //TODO restart timer
+    this.restoreHearts();
+    this.renderNewRound();
+  }
+
+  restoreHearts() {
+    const heartElements = $('.header__heart--empty');
+    for (let element of heartElements) {
+      element.classList.remove('header__heart--empty');
+      element.classList.add('header__heart--full');
+    }
+  }
+
+  handleTimer() {
+    clearInterval(window.id)
+    var elem = document.getElementById("myBar");
+    elem.style.width = '1%';
+    var width = 1;
+    window.id = setInterval( () => {
+        if (width >= 100) {
+          clearInterval(window.id);
+          this.endGame()
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }, 100)
   }
 }
 
@@ -120,12 +169,11 @@ var newGame = new Game()
 
 newGame.renderNewRound();
 
-
 setTimeout(function(){ newGame.endGame() }, 20000);
 
 
 
-//addeventlistener, game.checkMove
+//timer, minigra
 
 
 
--- out.js
+++ out.js
@@ -74,20 +74,6 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-// function move() {
-//     var elem = document.getElementById("myBar");
-//     var width = 1;
-//     var id = setInterval(frame, 10);
-//     function frame() {
-//         if (width >= 100) {
-//             clearInterval(id);
-//         } else {
-//             width++;
-//             elem.style.width = width + '%';
-//         }
-//     }
-// }
-
 function shuffle(a) {
   for (var i = a.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
@@ -98,30 +84,62 @@ function shuffle(a) {
   return a;
 }
 
-var PAIR_GROUPS = [['football', 'basketball', 'baseball'], ['picasso', 'dali', 'gogh'], ['chopin', 'mozart'], ['africa', 'asia'], ['pig', 'sheep', 'cow']];
+var PAIR_GROUPS = [['football', 'basketball', 'baseball'], ['picasso', 'dali', 'gogh'], ['pig', 'sheep', 'cow']];
+
+// ['chopin', 'mozart'],
+// ['africa', 'asia'],
 
 var Game = function () {
   function Game() {
     _classCallCheck(this, Game);
 
     this.lives = 3;
-    this.currentIndex = 1;
-    this.playTime = 20;
+    this.currentIndex = 0;
     this.selected = null;
   }
 
   _createClass(Game, [{
-    key: 'startGame',
-    value: function startGame() {}
+    key: 'removeSelection',
+    value: function removeSelection(arr) {
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
+
+      try {
+        for (var _iterator = arr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          var el = _step.value;
+
+          el.classList.remove('image-border');
+        }
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
+        }
+      }
+    }
   }, {
     key: 'renderNewRound',
     value: function renderNewRound() {
       var _this = this;
 
+      this.handleTimer();
+      $('.counter span')[0].innerHTML = this.currentIndex + 1;
+      this.selected = null;
       //dom elements
       var imageContainers = $('.image-container');
       var wordContainers = $('.body__word-field');
-
+      //TODO to delete later when animation
+      this.removeSelection(imageContainers);
+      //
       var toDisplayGroup = PAIR_GROUPS[this.currentIndex];
 
       var shuffledWords = toDisplayGroup.slice();
@@ -133,16 +151,23 @@ var Game = function () {
         var imageContainer = imageContainers[index];
         var wordContainer = wordContainers[index];
 
-        imageContainer.classList.add(toDisplayGroup[index]);
+        if (imageContainer.classList.length === 2) {
+          imageContainer.classList.remove(imageContainer.classList.item(1));
+          imageContainer.classList.add(toDisplayGroup[index]);
+        } else {
+          imageContainer.classList.add(toDisplayGroup[index]);
+        }
         imageContainer.dataset.name = toDisplayGroup[index];
         //add to dataset
         wordContainer.lastElementChild.innerHTML = shuffledWords[index];
+
         imageContainer.addEventListener('click', function () {
           _this.toggleBorder(imageContainer);
           _this.selected = imageContainer.dataset.name;
         });
 
-        wordContainer.addEventListener('click', function () {
+        wordContainer.addEventListener('click', function (e) {
+          e.stopImmediatePropagation();
           if (_this.selected) {
             _this.checkPair(wordContainer);
           }
@@ -160,44 +185,63 @@ var Game = function () {
   }, {
     key: 'checkPair',
     value: function checkPair(element) {
+      this.checkEndGame();
       var word = element.lastElementChild.innerHTML;
       if (this.selected === word) {
-        console.log('mamy parke');
+        if (this.currentIndex + 1 === PAIR_GROUPS.length) {
+          this.endGame();
+        } else {
+          this.currentIndex += 1;
+          this.renderNewRound();
+        }
+      } else {
+        this.looseLife();
       }
+    }
+  }, {
+    key: 'looseLife',
+    value: function looseLife() {
+      this.lives -= 1;
+      var hearts = $('.header__heart--full');
+
+      hearts[hearts.length - 1].classList.remove('header__heart--full');
+      hearts[hearts.length - 1].classList.add('header__heart--empty');
+      this.checkEndGame();
     }
   }, {
     key: 'toggleBorder',
     value: function toggleBorder(element) {
       if (this.selected && element.dataset.name !== this.selected) {
         var imageContainers = $('.image-container');
-        var _iteratorNormalCompletion = true;
-        var _didIteratorError = false;
-        var _iteratorError = undefined;
+        var _iteratorNormalCompletion2 = true;
+        var _didIteratorError2 = false;
+        var _iteratorError2 = undefined;
 
         try {
-          for (var _iterator = imageContainers[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            var container = _step.value;
+          for (var _iterator2 = imageContainers[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+            var container = _step2.value;
 
-            if (container.dataset.name === this.selected) {
-              container.style.border = 'none';
-            }
+            // if (container.dataset.name === this.selected) {
+            //   container.style.border = 'none';
+            // }
+            container.classList.remove('image-border');
           }
         } catch (err) {
-          _didIteratorError = true;
-          _iteratorError = err;
+          _didIteratorError2 = true;
+          _iteratorError2 = err;
         } finally {
           try {
-            if (!_iteratorNormalCompletion && _iterator.return) {
-              _iterator.return();
+            if (!_iteratorNormalCompletion2 && _iterator2.return) {
+              _iterator2.return();
             }
           } finally {
-            if (_didIteratorError) {
-              throw _iteratorError;
+            if (_didIteratorError2) {
+              throw _iteratorError2;
             }
           }
         }
       }
-      element.style.border = '7px solid black';
+      element.classList.add('image-border');
     }
   }, {
     key: 'checkMove',
@@ -205,14 +249,67 @@ var Game = function () {
   }, {
     key: 'checkEndGame',
     value: function checkEndGame() {
-      if (this.lives === 0 || this.playTime === 0) {
+      if (this.lives === 0 || this.currentIndex === PAIR_GROUPS.length) {
         this.endGame();
       }
     }
   }, {
     key: 'endGame',
     value: function endGame() {
-      console.log('game end');
+      this.currentIndex = 0;
+      this.lives = 3;
+      //TODO restart timer
+      this.restoreHearts();
+      this.renderNewRound();
+    }
+  }, {
+    key: 'restoreHearts',
+    value: function restoreHearts() {
+      var heartElements = $('.header__heart--empty');
+      var _iteratorNormalCompletion3 = true;
+      var _didIteratorError3 = false;
+      var _iteratorError3 = undefined;
+
+      try {
+        for (var _iterator3 = heartElements[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+          var element = _step3.value;
+
+          element.classList.remove('header__heart--empty');
+          element.classList.add('header__heart--full');
+        }
+      } catch (err) {
+        _didIteratorError3 = true;
+        _iteratorError3 = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion3 && _iterator3.return) {
+            _iterator3.return();
+          }
+        } finally {
+          if (_didIteratorError3) {
+            throw _iteratorError3;
+          }
+        }
+      }
+    }
+  }, {
+    key: 'handleTimer',
+    value: function handleTimer() {
+      var _this2 = this;
+
+      clearInterval(window.id);
+      var elem = document.getElementById("myBar");
+      elem.style.width = '1%';
+      var width = 1;
+      window.id = setInterval(function () {
+        if (width >= 100) {
+          clearInterval(window.id);
+          _this2.endGame();
+        } else {
+          width++;
+          elem.style.width = width + '%';
+        }
+      }, 100);
     }
   }]);
 
@@ -227,7 +324,7 @@ setTimeout(function () {
   newGame.endGame();
 }, 20000);
 
-//addeventlistener, game.checkMove
+//timer, minigra
 
 /***/ })
 /******/ ]);
